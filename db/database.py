9b11c7db6f62cb44efe8fdfeb749e2eebd546f8a
messineg <gregory.messine@gmail.com>
2024-11-13 09:14:04 +0100

    Aanpassingen aan database en main file

--- db/database.py
+++ db/database.py
@@ -48,13 +48,13 @@ def maak_tabellen_voeg_data_toe():
 
 	#Voeg regisseurs data toe aan de tabel
 	regisseurs_data = [('Christopher Nolan', 1970),('Steven Spielberg', 1946),('Quentin Tarantino', 1963),('Martin Scorsese', 1942),('James Cameron', 1954)]
-	cursor.executemany('INSERT INTO Regisseurs (naam, geboorte_jaar) VALUES (?, ?)', regisseurs_data)  
+	cursor.executemany('INSERT OR IGNORE INTO Regisseurs (naam, geboorte_jaar) VALUES (?, ?)', regisseurs_data)  
 	
 	print("Initiële data voor regisseurs werd toegevoegd.")
 	
 	#Voeg films toe en zoek naar de id van de regisseur op basis van de naam
 	cursor.execute('''
-        INSERT INTO Films (titel, release_jaar, genre, regisseur_id)
+        INSERT OR IGNORE INTO Films (titel, release_jaar, genre, regisseur_id)
         VALUES 
             ('Inception', 2010, 'Science Fiction', (SELECT id FROM Regisseurs WHERE naam = 'Christopher Nolan')),
             ('Schindler List', 1993, 'Drama', (SELECT id FROM Regisseurs WHERE naam = 'Steven Spielberg')),
--- main.py
+++ main.py
@@ -2,12 +2,26 @@ import sqlite3
 from db.database import maak_connectie, maak_tabellen_voeg_data_toe
 import json
 import os
+from functies.zoek_film import zoek_films_alle, zoek_film_vraag
 
 
 if __name__ == '__main__':
 	
-
 	maak_connectie()
 	maak_tabellen_voeg_data_toe()
 
 	print("De eerste basis is uitgevoerd")
+
+	acties = {
+    "1": zoek_films_alle,
+    "2": zoek_film_vraag
+	}
+
+	user_input= input("Geef een cijfer voor de functie die je wil laten uitvoeren: ")
+
+	if user_input in acties:
+		acties[user_input]()
+	else:
+   		print("Kies een ander cijfer")
+
+	
