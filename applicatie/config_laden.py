8bf47da6b3d637f2d73000eecbb84aa4d4a026fb
messineg <gregory.messine@gmail.com>
2024-11-23 12:45:24 +0100

    Tussentijdse commit voor pauze

--- applicatie/config_laden.py
+++ applicatie/config_laden.py
@@ -6,9 +6,14 @@ def laad_config():
 	huidige_map = os.path.dirname(os.path.abspath(__file__))
 	pad_naar_config = os.path.join(huidige_map,"..","config.json")
 
-	#config.json inladen
-	with open(pad_naar_config) as bestand:
-		config = json.load(bestand)
-	return config
+	#config.json inladen met ingebouwde foutafhandeling wanneer bestand niet gevonden wordt of er zich een algemene fout voordoet
+	try:
+		with open(pad_naar_config, "r") as bestand:
+			config = json.load(bestand)
+		return config
+	except FileNotFoundError:
+		print(f"Het bestand config.json werd niet gevonden op pad: {pad_naar_config}")
+	except Exception as error:
+		print(f"Er deed zich een fout voor: {error}")
 
 	
